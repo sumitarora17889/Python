a2d11f2b93239c635d99ae2c0b7f37c0f033eaf8
Sumit Arora <sumit.arora@indiamart.com>
2019-09-02 12:37:56 +0530

    SVD current version

--- algorithms/vectors/SVD1.py
+++ algorithms/vectors/SVD1.py
@@ -1,31 +1,53 @@
 import numpy as np
-
 def diagonal(array, row, column):
     Diag=[]
     for i in range(row):
+        row=[]
         for j in range(column):
             if i==j:
-                Diag[i][j]=array[i]
+                row.append(array[i])
             else:
-                Diag[i][j]=0
+                row.append(0)
+        Diag.append(row)
     return Diag
 
 A=np.random.randint(1,4,size=[4,5])
+print("========A========\r\n")
 print(A)
+print("========A========\r\n")
 
 AAT=np.matmul(A,A.T)
-
+print("========AAT========\r\n")
 print(AAT)
+print("========AAT========\r\n")
 
 ATA=np.matmul(A.T,A)
+print("========ATA========\r\n")
 
 print(ATA)
-evalues,evectors=np.linalg.eig(AAT)
+print("========ATA========\r\n")
 
+evalues,evectors=np.linalg.eig(AAT)
+print("========Evalues========\r\n")
+
+print(evalues)
+
+print("========Evalues========\r\n")
+
+print("========evectors========\r\n")
+print(evectors)
+print("========evectors========\r\n")
 
 index=np.argsort(evalues)
+print("========Descending Sorting Index reverse========\r\n")
 print(index[::-1])
-print(index)
+print("========Descending Sorting Index reverse========\r\n")
+
+# print("========Sorting Index========\r\n")
+#
+# print(index)
+# print("========Sorting Index========\r\n")
+
 S=evalues[index[::-1]]
 
 U=evectors[index[::-1]].T
@@ -35,18 +57,40 @@ index=np.argsort(evalues)
 S1=evalues[index[::-1]]
 V=evectors[index[::-1]]
 
+
+print("========Eigens========\r\n")
+print("========U========\r\n")
+
 print(U)
-print(np.sqrt(S))
-print(np.sqrt(S1))
+print("========U========\r\n")
+print("========S========\r\n")
+S=np.sqrt(S)
+print(S)
+print("========S========\r\n")
+
+print("========V========\r\n")
+
 print(V)
+print("========V========\r\n")
+SSVD=diagonal(S,4,5)
+print(np.matmul(np.matmul(U.T,SSVD),V))
 
 US,SS,VS=np.linalg.svd(A)
+print("========SVD Formula========\r\n")
 
-print("tes")
+print("========U SVD Formula========\r\n")
+
 print(US)
+print("========U SVD Formula========\r\n")
+print("========Sigma SVD Formula========\r\n")
+SSVD=diagonal(SS,4,5)
 print(SS)
-print(VS)
+print("========Sigma SVD Formula========\r\n")
+print("========V SVD Formula========\r\n")
 
-SSVD=diagonal(SS, 4,5)
-print(np.matmul(np.matmul(US,SSVD),VS).astype(int))
+print(VS)
+print("========V SVD Formula========\r\n")
+
+# SSVD=diagonal(SS, 4,5)
+print(np.matmul(np.matmul(US,SSVD),VS))
 
