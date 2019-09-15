f6f81ff4d2b87d297c836e60d4c77a22d0c7da6a
Sumit Arora <sumit.arora@indiamart.com>
2019-09-15 23:20:08 +0530

    Quicksort completed

--- algorithms/vectors/SVD1.py
+++ algorithms/vectors/SVD1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 def diagonal(array, row, column):
     Diag=[]
     for i in range(row):
@@ -11,79 +12,70 @@ def diagonal(array, row, column):
         Diag.append(row)
     return Diag
 
-# A=np.random.randint(1,4,size=[4,5])
+A=np.random.randint(1,4,size=[4,5])
 A=np.matrix([[2,4],[1,3],[0,0],[0,0]])
 print("========A========\r\n")
 print(A)
 print("========A========\r\n")
 
 AAT=np.matmul(A,A.T)
-print("========AAT========\r\n")
-print(AAT)
-print("========AAT========\r\n")
+# print("========AAT========\r\n")
+# print(AAT)
+# print("========AAT========\r\n")
 
 ATA=np.matmul(A.T,A)
-print("========ATA========\r\n")
-
-print(ATA)
-print("========ATA========\r\n")
+# print("========ATA========\r\n")
+#
+# print(ATA)
+# print("========ATA========\r\n")
 
 evalues,evectors=np.linalg.eig(AAT)
-print("========Evalues========\r\n")
+# print("========Evalues========\r\n")
+# print(evalues)
+# print("\r\n")
+sort_index=np.argsort(evalues)
+sort_index=np.flip(sort_index)
+Sigma=diagonal(np.sqrt(evalues[sort_index]),4,2)
+# print(Sigma)
 
-print(evalues)
 
-print("========Evalues========\r\n")
+# print("========Evalues========\r\n")
+# print("========evectors========\r\n")
+U=evectors[sort_index]
+# print(U)
+# print("========evectors========\r\n")
 
-print("========evectors========\r\n")
-print(evectors)
-print("========evectors========\r\n")
 
-index=np.argsort(evalues)
-print("========Descending Sorting Index reverse========\r\n")
-print(index[::-1])
-print("========Descending Sorting Index reverse========\r\n")
-
-# print("========Sorting Index========\r\n")
-#
-# print(index)
-# print("========Sorting Index========\r\n")
-
-S=evalues[index[::-1]]
-
-U=evectors[index[::-1]]
 evalues,evectors=np.linalg.eig(ATA)
 
-index=np.argsort(evalues)
-S1=evalues[index[::-1]]
-V=evectors[index[::-1]]
 
+# print("========Eigens========\r\n")
+# print("========Evalues========\r\n")
+# print(evalues)
+sort_index=np.argsort(evalues)
+sort_index=np.flip(sort_index)
+V=evectors.T[sort_index]
+# print(evalues[sort_index])
+#
+# print("========Evalues========\r\n")
+#
+# print("========evectors========\r\n")
+# print(evectors)
+# print(V)
+# print("========evectors========\r\n")
 
-print("========Eigens========\r\n")
-print("========Evalues========\r\n")
-
-print(evalues)
-
-print("========Evalues========\r\n")
-
-print("========evectors========\r\n")
-print(evectors)
-print("========evectors========\r\n")
 print("========U========\r\n")
-
 print(U)
+# print(np.matmul(np.matmul(U,Sigma),V.T))
 print("========U========\r\n")
-print("========S========\r\n")
-S=np.sqrt(S)
-print(S)
-print("========S========\r\n")
-
+print("========Sigma========\r\n")
+print(Sigma)
+# print(np.matmul(np.matmul(U,Sigma),V.T))
+print("========Sigma========\r\n")
 print("========V========\r\n")
-
 print(V)
+print(np.matmul(np.matmul(U,Sigma),V.T))
 print("========V========\r\n")
-SSVD=diagonal(S,4,2)
-print(np.matmul(np.matmul(U.T,SSVD),V))
 
 US,SS,VS=np.linalg.svd(A)
 print("========SVD Formula========\r\n")
@@ -94,13 +86,13 @@ print(US)
 print("========U SVD Formula========\r\n")
 print("========Sigma SVD Formula========\r\n")
 SSVD=diagonal(SS,4,2)
-print(SS)
+print(SSVD)
 print("========Sigma SVD Formula========\r\n")
 print("========V SVD Formula========\r\n")
 
-print(VS)
+print(VS.T)
 print("========V SVD Formula========\r\n")
 
 # SSVD=diagonal(SS, 4,5)
-print(np.matmul(np.matmul(US,SSVD),VS))
+print(np.matmul(np.matmul(US,SSVD),VS.T))
 
