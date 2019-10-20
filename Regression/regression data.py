5cf86eeb2ded601434397acaeede00c9e4809ec2
Sumit Arora <sumit.arora@indiamart.com>
2019-10-20 20:48:14 +0530

    Changes in Regression

--- Regression/regression data.py
+++ Regression/regression data.py
@@ -12,23 +12,14 @@ plt.scatter(X,Y)
 residue=[]
 slope=[]
 intercept=[]
-for i in range(1,6):
-    plt.scatter(X, Y)
-    Xpow=np.power(X,i)
-    A = np.vstack([Xpow, np.ones(len(Xpow))]).T
-    model = np.linalg.lstsq(A, Y, rcond=None)
-    slope.append(model[0][0])
-    intercept.append(model[0][1])
-    residue.append(model[1][0])
-    Ypred=model[0][1] + model[0][0]*Xpow
-    plt.plot(X,Ypred)
-    plt.xlabel('x^%s - axis' % i)
-    plt.ylabel('y - axis')
-    plt.title('Regression X^%s-Y' % i)
-    plt.show()
-sort_index=np.argsort(residue,kind = 'stable')
-print(sort_index)
+plt.scatter(X, Y)
+A = np.vstack([X, np.ones(len(X))]).T
+model = np.linalg.lstsq(A, Y, rcond=None)
+Ypred=model[0][1] + model[0][0]*X
+plt.plot(X,Ypred)
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+plt.title('Regression X-Y')
+plt.show()
 print('In the order of fitness')
-for i in sort_index:
-    print(i)
-    print('X^%s vs Y\r\nResidue: %s\t Equation: Y=%s+%sX^%s' %(i,residue[i],intercept[i],slope[i],i))
+print('X vs Y\r\nResidue: %s\t Equation: Y=%s+%sX' %(model[1][0],model[0][1],model[0][0]))
