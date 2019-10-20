cd242fde045ba92eeeddb7773566315d396182d6
Sumit Arora <sumit.arora@indiamart.com>
2019-10-20 19:35:32 +0530

    Root 7

---
+++ Experiments/Covariance.py
@@ -0,0 +1,19 @@
+# import numpy as np
+
+# x=np.arange(30)
+# y = [39343,46205,37731,43525,39891,56642,60150,54445,64445,57189,63218,55794,56957,57081,61111,67938,66029,83088,81363,93940,91738,98273,101302,113812,109431,105582,116969,112635,122391,121872]
+#
+# np.mean(x)
+
+import pandas as pd
+import numpy as np
+import copy
+
+a = [1, 2, 3]
+a[0] = a
+print(a)
+print(*a)
+
+x = np.ones(6)
+print(x.shape)
+print(x.T.shape)
