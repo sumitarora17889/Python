cfebad6e024fbeacbce46313ff1fd1f4b4d7426e
Sumit Arora <sumit.arora@indiamart.com>
2019-10-20 20:39:50 +0530

    Changes in Regression

--- Regression/regression data.py
+++ Regression/regression data.py
@@ -1,11 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-X=[1,1.5,2,2.5,3]
+X=np.arange(30)
+Y = [39343,46205,37731,43525,39891,56642,60150,54445,64445,57189,63218,55794,56957,57081,61111,67938,66029,83088,81363,93940,91738,98273,101302,113812,109431,105582,116969,112635,122391,121872]
 
-Y=[1,5,18,36,80]
+plt.scatter(X,Y)
 
-# plt.plot(X,Y)
-#
 # plt.xlabel('x - axis')
 # plt.ylabel('y - axis')
 # plt.title('Regression X-Y')
@@ -14,17 +13,19 @@ residue=[]
 slope=[]
 intercept=[]
 for i in range(1,6):
+    plt.scatter(X, Y)
     Xpow=np.power(X,i)
-    plt.plot(Xpow,Y)
-    plt.xlabel('x^%s - axis'%i)
-    plt.ylabel('y - axis')
-    plt.title('Regression X^%s-Y'%i)
-    plt.show()
     A = np.vstack([Xpow, np.ones(len(Xpow))]).T
     model = np.linalg.lstsq(A, Y, rcond=None)
     slope.append(model[0][0])
     intercept.append(model[0][1])
     residue.append(model[1][0])
+    Ypred=model[0][1] + model[0][0]*Xpow
+    plt.plot(X,Ypred)
+    plt.xlabel('x^%s - axis' % i)
+    plt.ylabel('y - axis')
+    plt.title('Regression X^%s-Y' % i)
+    plt.show()
 sort_index=np.argsort(residue,kind = 'stable')
 print(sort_index)
 print('In the order of fitness')
