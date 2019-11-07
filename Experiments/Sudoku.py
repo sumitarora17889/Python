dfe9a80022398f17878f3d3287664f39324c6b67
Sumit Arora <sumit.arora@indiamart.com>
2019-11-07 20:11:09 +0530

    Sudoku Update

--- Experiments/Sudoku.py
+++ Experiments/Sudoku.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-matrix = np.full((9, 9, 9), True)
+set=set(np.arange(1,10))
+matrix = np.full((9, 9), set)
+print(matrix)
 
-
-def printsudoku(array):
+def printsudoku(matrix):
     fig = plt.figure()
     ax = fig.gca()
     plt.gca().invert_yaxis()
@@ -14,8 +15,10 @@ def printsudoku(array):
     plt.grid()
     ax.set_yticklabels([])
     ax.set_xticklabels([])
-    for i in range(len(array)):
-        plt.text((array[i][1] - 0.6), (array[i][0] - 0.3), array[i][2])
+    for i in range(9):
+        for j in range(9):
+            plt.text((j+0.4), (i +0.7), matrix[i][j])
+    plt.show()
 
 
 # entries = int(input('Enter Number of Entries:'))
@@ -24,11 +27,11 @@ array = [[1, 4, 6], [1, 6, 4], [1, 7, 7], [2, 1, 7], [2, 3, 6], [2, 9, 9], [3, 6
          [4, 8, 9], [4, 9, 3], [5, 1, 8], [5, 9, 5], [6, 1, 4], [6, 2, 3], [6, 5, 1], [6, 8, 7], [7, 2, 5], [7, 4, 2],
          [8, 1, 3], [8, 7, 2], [8, 9, 8], [9, 3, 2], [9, 4, 3], [9, 6, 1]]
 
-printsudoku(array)
+printsudoku(matrix)
 
 
 def adjust(matrix, row, col, value):
-    if matrix[row-1][col-1][value-1]:
+    if value in matrix[i][j]:
         g1 = divmod(row-1, 3)[0] * 3
         g2 = divmod(col-1, 3)[0] * 3
         matrix[row - 1,:,value - 1] = False
@@ -47,8 +50,8 @@ def solvesudoku(array):
             return False
     return True
 
-solvesudoku(array)
-
-for i in range(9):
-    for j in range(9):
-        print(np.arange(1,10)[matrix[i][j]])
+# solvesudoku(array)
+#
+# for i in range(9):
+#     for j in range(9):
+#         print(np.arange(1,10)[matrix[i][j]])
