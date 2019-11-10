e61c7d29d6381fa9c819cd2ee2eb4d0a20325a38
Sumit Arora <sumit.arora@indiamart.com>
2019-11-11 02:01:06 +0530

    Sudoku

--- Experiments/KaratSuba.py
+++ Experiments/KaratSuba.py
@@ -1,43 +1,43 @@
 import math
 from datetime import datetime
-177648187715183390
-num1='5455455'
-num2='754487458'
-print(num1)
-print(num2)
+num1=9876789
+num2=6756321
 
-
-def sum(*args):
-    sumval=0
-    for a in args:
-        sumval=sumval+int(a)
-    return str(sumval)
-
-def subtract(a,b,c):
-    return str(int(a)-int(b)-int(c))
+# def sum(*args):
+#     sumval=0
+#     for a in args:
+#         sumval=sumval+int(a)
+#     return str(sumval)
+#
+# def subtract(a,b,c):
+#     return str(int(a)-int(b)-int(c))
 
 def multiply(num1,num2):
-    num1 = num1.rjust(len(num2), '0')
-    num2 = num2.rjust(len(num1), '0')
-    if len(num1)==0:
-        return ''
-    if len(num1)==1:
-        return str(int(num1)*int(num2))
-    mid = math.trunc((len(num2))/ 2)
-    firsts=multiply(num1[:mid],num2[:mid])
-    firstsecond=multiply(num1[mid:],num2[mid:])
-    answer=sum(     firsts+('0'*((len(num1)-mid)*2)),   subtract(multiply(sum(num1[:mid],num1[mid:]),sum(num2[:mid],num2[mid:])),firstsecond,firsts)+('0'*(len(num1)-mid)),     firstsecond)
+    bitlength=max(num1.bit_length(),num2.bit_length())
+    if bitlength==0 or num1==0 or num2==0:
+        return 0
+    if bitlength==1:
+        return num1*num2
+    mid=bitlength>>1
+    num11=num1>>mid
+    mask=(1<<mid)-1
+    num12=num1 & mask
+    num21=num2>>mid
+    num22=num2 & mask
+    firsts=multiply(num11,num21)
+    firstsecond=multiply(num12,num22)
+    answer=(firsts<<(mid<<1))+  ((multiply((num11+num12),(num21+num22))-firstsecond-firsts)<<mid)+     firstsecond
     return answer
 
 start = datetime.now()
 for i in range(5000):
     a=multiply(num1,num2)
 end=datetime.now()
-print(end-start)
+print((a,end-start))
 
 start = datetime.now()
 for i in range(5000):
-    a=5455455*754487458
+    a=num1*num2
 end=datetime.now()
-print(end-start)
+print((a,end-start))
 
