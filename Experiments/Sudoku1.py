99e48d0b5cb4d58a6c47a8dbc4e7f18dfd14a497
Sumit Arora <sumit.arora@indiamart.com>
2019-11-14 08:25:26 +0530

    Sudoku Update

--- Experiments/Sudoku1.py
+++ Experiments/Sudoku1.py
@@ -30,42 +30,76 @@ def adjust(a):
     for i in range(3):
         grids[value-1][rowdiv*3+i]=grids[value-1][rowdiv*3+i] - set(np.arange(9)[divmod(np.arange(9),3)[0]==rowmod])
         grids[value-1][3*i+coldiv]=grids[value-1][3*i+coldiv] - set(np.arange(9)[divmod(np.arange(9),3)[1]==colmod])
+        rows[value-1][rowdiv*3+i]=rows[value-1][rowdiv*3+i]- set(np.arange(9)[divmod(np.arange(9),3)[0]==coldiv])
+        columns[value - 1][coldiv * 3 + i] = rows[value - 1][coldiv * 3 + i] - set(
+            np.arange(9)[divmod(np.arange(9), 3)[0] == rowdiv])
     for i in range(9):
         columns[value-1][i]=columns[value-1][i] - {row}
         rows[value-1][i]=rows[value-1][i] - {col}
         grids[i][grid]=grids[i][grid]-{pos}
     grids[value-1][grid]={pos}
-    rows[value-1][col]={row}
-    columns[value-1][row] ={col}
+    rows[value-1][row]={col}
+    columns[value-1][col] ={row}
 
 def insert(a):
     matrix[a[0]-1][a[1]-1]=a[2]
     adjust(a)
 
+def printsudoku(matrix):
+    fig = plt.figure()
+    ax = fig.gca()
+    plt.gca().invert_yaxis()
+    ax.set_xticks(np.arange(1, 10, 1), minor=True)
+    ax.set_yticks(np.arange(0, 10, 1), minor=True)
+    ax.grid(which='minor', lw=20)
+    plt.grid()
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
+    for i in range(9):
+        for j in range(9):
+            plt.text((j+0.4), (i +0.7), matrix[i][j])
+    plt.show()
+
 for a in array:
     insert(a)
 print(grids)
+printsudoku(matrix)
 for i in range(9):
-    value=i+1
-    for j in range(9):
-        if len(grids[i][j])==0:
-            print("No Solution Found")
-        elif len(grids[i][j])==1:
-            pos=grids[i][j].pop()
-            rowval,colval=getpos(j,pos)
-            if matrix[rowval][colval]==0:
-                insert((rowval+1,colval+1,value))
-            elif matrix[rowval][colval]!=value:
-                print("Solution Error")
-        if len(rows[i][j])==0:
-            print("No Solution Found")
-        elif len(rows[i][j])==1:
-            colval=rows[i][j].pop()
-            rowval=j
-            if matrix[rowval][colval]==0:
-                insert((rowval+1,colval+1,value))
-            elif matrix[rowval][colval]!=value:
-                print("Solution Error")
+    for i in range(9):
+        value=i+1
+        for j in range(9):
+            if len(grids[i][j])==0:
+                print("No Solution Found")
+            elif len(grids[i][j])==1:
+                pos=grids[i][j].pop()
+                grids[i][j].add(pos)
+                rowval,colval=getpos(j,pos)
+                if matrix[rowval][colval]==0:
+                    insert((rowval+1,colval+1,value))
+                elif matrix[rowval][colval]!=value:
+                    print("Solution Error")
+            if len(rows[i][j])==0:
+                print("No Solution Found")
+            elif len(rows[i][j])==1:
+                colval=rows[i][j].pop()
+                rows[i][j].add(colval)
+                rowval=j
+                if matrix[rowval][colval]==0:
+                    insert((rowval+1,colval+1,value))
+                elif matrix[rowval][colval]!=value:
+                    print("Solution Error")
+            if len(columns[i][j])==0:
+                print("No Solution Found")
+            elif len(columns[i][j])==1:
+                rowval=columns[i][j].pop()
+                columns[i][j].add(rowval)
+                colval=j
+                if matrix[rowval][colval]==0:
+                    insert((rowval+1,colval+1,value))
+                elif matrix[rowval][colval]!=value:
+                    print("Solution Error")
 
 print(matrix)
-print(grids[1])
+print(grids)
+printsudoku(matrix)
+print(len(np.nonzero(matrix)[0]))
